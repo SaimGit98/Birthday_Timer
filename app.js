65d4aebe5903c579e4ef194190ee72ab4a711f7f
Muhammad Saim <141481171+SaimGit98@users.noreply.github.com>
2023-11-02 09:30:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let daybox = document.getElementById("day")
+let expectingDate = new Date("28,February,2024 00:00:00").getTime();
+console.log(expectingDate);
+let x = setInterval(function(){
+   let now = new Date().getTime();
+   let distance = expectingDate - now ;
+
+   var days = Math.floor(distance /(1000 * 60 * 60  *24));
+   var hours = Math.floor((distance % (1000 * 60 * 60  * 24)) /  (1000 * 60 * 60));
+   var minutes = Math.floor((distance % ( 1000 * 60 * 60 )) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60 )) / 1000);
+ 
+   document.getElementById("day").innerHTML = days;
+   document.getElementById("hours").innerHTML = hours ;
+   document.getElementById("minutes").innerHTML = minutes;
+   document.getElementById("seconds").innerHTML = seconds;
+
+     document.getElementById("birthday").innerHTML = days + "days" ;
+    
+} , 1000)
